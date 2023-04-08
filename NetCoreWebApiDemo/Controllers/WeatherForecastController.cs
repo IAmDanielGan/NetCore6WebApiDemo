9c70e687b5a0066df62e0526f3ec38f7ccfef805
IAmDanielGan <ganyuansong@gmail.com>
2023-04-08 00:09:13 -0700

    Add Post Method

--- NetCoreWebApiDemo/Controllers/WeatherForecastController.cs
+++ NetCoreWebApiDemo/Controllers/WeatherForecastController.cs
@@ -18,8 +18,24 @@ namespace NetCoreWebApiDemo.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public IEnumerable<WeatherForecast> Post()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
